63ff338ffd38f7d6327b18dcd74f03f99e5b5277
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-05-25 21:25:49 -0500

    Sonarline

--- src/main/java/com/sofka/proyect/App.java
+++ src/main/java/com/sofka/proyect/App.java
@@ -1,8 +1,24 @@
 package com.sofka.proyect;
 
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        Logger logger = Logger.getLogger(App.class.getName());
+        Scanner entrada = new Scanner(System.in);
+
+        logger.info("Seleccione Una Opcion de la Calculadora:");
+        int option = entrada.nextInt();
+
+        if(option == 1){
+            logger.info("Esto es una suma");
+        }
+        if(option == 2){
+            logger.info("Esto es una Resta");
+        }
+
+        entrada.close();
     }
 }
