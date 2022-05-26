4ea5fae7dd51b6b16f350f305ae275b555bf7146
felipemarin93 <felipemaringiraldo@gmail.com>
2022-05-25 22:02:20 -0500

    Se agregan los métodos de sumar y restar

--- src/main/java/com/sofka/proyect/CalculatorApp.java
+++ src/main/java/com/sofka/proyect/CalculatorApp.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class CalculatorApp {
-    public static void main(String[] args) {
+    public double addNumbers(double numberOne,double numberTwo){
+        return numberOne + numberTwo;
+    }
 
+    public double substract(double numberOne,double numberTwo){
+        return numberOne - numberTwo;
+    }
 }
