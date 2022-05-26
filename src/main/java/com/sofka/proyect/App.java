da07f2459c50d8f43d032868d8a276ae002fb6fb
felipemarin93 <felipemaringiraldo@gmail.com>
2022-05-25 23:51:00 -0500

    Se realiza la documentación de la clase principal de la app

--- src/main/java/com/sofka/proyect/App.java
+++ src/main/java/com/sofka/proyect/App.java
@@ -8,6 +8,10 @@ public class App {
         Scanner inputValue = new Scanner(System.in);
         CalculatorApp operation = new CalculatorApp();
 
+        /*
+        Se declaran las variables que van a almacenar los dos números de tipo double para que permita el ingreso de números
+        decimales, se especifica que el valor será asignado por el usuario a través de teclado
+         */
         double numberOne;
         double numberTwo;
 
@@ -25,8 +29,17 @@ public class App {
 
         logger.info("1: Sumar \n 2: Restar \n 3: Multiplicar \n 4: Dividir");
 
+        /*
+        Esta variable va a almacenar la opción seleccionada por el usuario.
+         */
         int option = inputValue.nextInt();
 
+
+        /*
+        Esta ciclo if dependera del valor recibido por teclado en el cual  se decide que operación se desea realizar
+        Al indicar que operación se va a efectuar se usa el objeto de la clase CalculatorApp instanciado para acceder a los métodos
+        para cada operación se envían los dos parametros tipo double correspondientes a ambos números.
+         */
         if(option == 1){
             logger.info("Esto es una suma");
             operation.addNumbers(numberOne,numberTwo);
@@ -39,7 +52,6 @@ public class App {
         else if (option == 3){
             logger.info("Esto es una Multiplicación");
             operation.multiplier(numberOne,numberTwo);
-
 
         }else if (option == 4){
             logger.info("Esto es una División");
