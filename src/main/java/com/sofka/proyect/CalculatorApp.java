337179d2f4c9297558be7fab1f194aeab73cbb64
felipemarin93 <felipemaringiraldo@gmail.com>
2022-05-25 22:12:56 -0500

    Se agrega documentación a los metodos de sumar y restar

--- src/main/java/com/sofka/proyect/CalculatorApp.java
+++ src/main/java/com/sofka/proyect/CalculatorApp.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class CalculatorApp {
+
+    /**
+     Método que recibe dos números tipo double para realizar la suma entre ellos y
+     @return El resultado de dicha operación
+     */
     public double addNumbers(double numberOne,double numberTwo){
         return numberOne + numberTwo;
     }
-
+    /**
+     Método que recibe dos números tipo double para realizar la resta entre ellos y
+     @return El resultado de dicha operación
+     */
     public double substract(double numberOne,double numberTwo){
         return numberOne - numberTwo;
     }
