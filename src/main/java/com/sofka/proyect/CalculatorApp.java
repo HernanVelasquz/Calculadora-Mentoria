410eca9be28381a30acd94640655a6d5464b2eff
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-05-25 21:57:06 -0500

    Agregando el metodo de Dividir en la clase Caluladora

---
+++ src/main/java/com/sofka/proyect/CalculatorApp.java
@@ -0,0 +1,11 @@
+package com.sofka.proyect;
+
+public class CalculatorApp {
+    public double multiplier(double numberOne, double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public double split(double numberOne, double numberTwo){
+        return numberOne / numberTwo;
+    }
+}
