1ec049e3992062c15c9ffca587646b4c21662bc8
felipemarin93 <felipemaringiraldo@gmail.com>
2022-05-25 21:49:01 -0500

    Se crea el Menu que solicita la operacion a realizar

--- src/main/java/com/sofka/proyect/CalculatorApp.java
+++ src/main/java/com/sofka/proyect/CalculatorApp.java
@@ -6,19 +6,5 @@ import java.util.logging.Logger;
 
 public class CalculatorApp {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(CalculatorApp.class.getName());
-        Scanner entrada = new Scanner(System.in);
 
-        logger.info("Seleccione Una Opcion de la Calculadora:");
-        int option = entrada.nextInt();
-
-        if(option == 1){
-            logger.info("Esto es una suma");
-        }
-        if(option == 2){
-            logger.info("Esto es una Resta");
-        }
-
-        entrada.close();
-    }
 }
