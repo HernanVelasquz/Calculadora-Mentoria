41742fc2e70aeaa8bbbc0654f155bee2b4921cfe
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-05-25 21:55:15 -0500

    Creando el metodo multiplicar en la clase Caluladora

--- src/main/java/com/sofka/proyect/App.java
+++ src/main/java/com/sofka/proyect/App.java
@@ -1,8 +1,22 @@
 package com.sofka.proyect;
-
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        Logger logger = Logger.getLogger(CalculatorApp.class.getName());
+        Scanner entrada = new Scanner(System.in);
+
+        logger.info("Seleccione Una Opcion de la Calculadora:");
+        int option = entrada.nextInt();
+
+        if(option == 1){
+            logger.info("Esto es una suma");
+        }
+        else if(option == 2){
+            logger.info("Esto es una Resta");
+        }
+
+        entrada.close();
     }
 }
